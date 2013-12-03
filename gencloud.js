2a0b3e93f3158e9aa703b464e4bb5b2bc823d9ac
Henry Phu <hphu@bu.edu>
2013-12-03 00:46:01 -0500

    updated html & input

--- gencloud.js
+++ gencloud.js
@@ -1,24 +1,31 @@
-  
+
+//  var tumblr = require('tumblr.js');
 
 
   //var color = d3.scale.ordinal().range(["#F77825","#D3CE3D","#F1EFA5", "#60B99A", "#CC2A41"]);
- var color = d3.scale.category20b();
+  
+  var color = d3.scale.category20b();
   var words = [{ "text": "Heero", "size": 1 }, { "text": "Villain", "size": 6 }, { "text": "Comics", "size": 4 }, { "text": "fire", "size": 3 }, {"text": "dragons", "size":1},
   { "text": "This", "size": 1 },{ "text": "soul", "size": 1 },{ "text": "death", "size": 3 },{ "text": "wounded", "size": 2 },{ "text": "try", "size": 1 },{ "text": "king", "size": 1 }, { "text": "archer", "size": 1 }, { "text": "tribute", "size": 2 },{ "text": "hello", "size": 2 },{ "text": "man", "size": 1 }, { "text": "rescue", "size": 2 },
   { "text": "lol", "size": 2 },{ "text": "knight", "size": 1 },{ "text": "school", "size": 1 },{ "text": "dream", "size": 1 },{ "text": "gladiator", "size": 2 },{ "text": "duel", "size": 2 }];
 
-  
+
   var width = Math.sqrt(words.length)*80, height = Math.sqrt(words.length)*60;
   var fontsize = d3.scale.log().range([18,80]);
+  var cloud;
 
-  console.log(width + " " + height);
-  d3.layout.cloud().size([width, height])
-      .words(words)
-      .font("Impact")
-      .fontSize(function(d) { return fontsize(d.size); })
-      .rotate(function() { return 0; })
-      .on("end", draw)
-      .start();
+  function createcloud(){
+    d3.select("svg")
+       .remove();
+    words = [{ "text": "Heero", "size": 1 }, { "text": "Villain", "size": 6 }];
+    cloud = d3.layout.cloud().size([width, height])
+        .words(words)
+        .font("Impact")
+        .fontSize(function(d) { return fontsize(d.size); })
+        .rotate(function() { return 0; })
+        .on("end", draw)
+        .start();
+      }
 
   function draw(words) {
     d3.select("#canvas").append("svg")
