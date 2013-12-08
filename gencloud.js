6c7b5f234f1fbba31274e40b1f21ec174ef6b9c8
Henry Phu <hphu@bu.edu>
2013-12-08 13:13:44 -0500

    added call for more posts, scaled domain and filtering words

--- gencloud.js
+++ gencloud.js
@@ -10,37 +10,49 @@
   var key = 'v0mQ8w4YAXML1K79SzARtQLhK0K2dPLWiMQUn2p9nQbSmtPJ15';
   var filter = 'text';
 
+  var stopwords = /the|you|and|with|from|about|above|any|but|across|not|now|would|like|how|has|two|one|all|also|among|anyhow|are|around|back|became|become|replied|becomes|yourself|beside|each|else|just|from|only|than|then|was|were|that|itself|these|into|him|himself|her|herself|while|during|before|after|they|their|themselves|then|i'm|i'll|i'd|i've|he'd|he'll|she'd|she|she'll|he's|she's|we've|they've|it's|its|isn't|aren't|wasn't|weren't|his|this|did|have|can|for|what|when|our|most|have|even|should/
+  var maxwords = 200;
 
-function tumbl(blogname){
-    $.ajax({
-      url: 'http://api.tumblr.com/v2/blog/'+blogname+'/posts?api_key='+key+'&filter='+filter,
-      dataType: "jsonp",
-      notes_info: 'false',
-      async: false,
-      success: function(data) {
-        var text = {};
-        for (var i=0;i<20;i++){
-          text = parsetumbl(data.response.posts[i], data.response.posts[i].type, text);
-        }
-        var jsonwords = [];
-        for (var i in text){
-          jsonwords.push({"text": i, "size": text[i]});
-        }
-        var words = jsonwords;
+  var text = {};
+function tumbl(blogname, callcount){
+      $.ajax({
+        url: 'http://api.tumblr.com/v2/blog/'+blogname+'/posts?api_key='+key+'&filter='+filter+'&offset='+callcount*20,
+        dataType: "jsonp",
+        notes_info: 'false',
+        async: false,
+        success: function(data) {
+          for (var i=0;i<20;i++){
+            text = parsetumbl(data.response.posts[i], data.response.posts[i].type, text);
+          }
+          callcount++;
+          if (callcount==2){
+            var jsonwords = [];
+            for (var i in text){
+              jsonwords.push({"text": i, "size": text[i]});
+            }
+            jsonwords = sortbycount(jsonwords).slice(0,maxwords);
+            var words = jsonwords;
+            width = Math.sqrt(words.length)*100;
+            height = Math.min(Math.sqrt(words.length)*60,650);
+            fontsize = d3.scale.log().domain([jsonwords[jsonwords.length-1].size,jsonwords[0].size]).range([17,70]);
+            var cloud = d3.layout.cloud().size([width, height])
+            .timeInterval(Infinity)
+            .words(words)
+            .font("Impact")
+            .fontSize(function(d) { return fontsize(d.size); })
+            .rotate(function() { return 0; })
+            .on("end", draw)
+            .start();
+           } else {
+            tumbl(blogname, callcount);
+           }
+         }
+      });
+  }
 
-        width = Math.sqrt(words.length)*100;
-        height = Math.min(Math.sqrt(words.length)*60,650);
-        fontsize = d3.scale.log().range([18,80]);
-
-        var cloud = d3.layout.cloud().size([width, height])
-        .timeInterval(Infinity)
-        .words(words)
-        .font("Impact")
-        .fontSize(function(d) { return fontsize(d.size); })
-        .rotate(function() { return 0; })
-        .on("end", draw)
-        .start();
-       }
+  function sortbycount(input){
+     return input.sort(function(a, b) {
+        return ((a["size"] < b["size"]) ? 1 : ((a["size"] > b["size"]) ? -1 : 0));
     });
   }
 
@@ -51,24 +63,33 @@ function tumbl(blogname){
         line = post.caption.split(/\s+/);
         break;
       case("text"):
-        line = (post.title + post.body).split(/\s+/);
-        console.log(line);
+        line = (post.title +" "+ post.body).split(/\s+/);
         break;
       case("quote"):
         line = (post.text.split(/\s+/));
         break;
       case("link"):
-        line = (post.description + post.title).split(/\s+/);
+        line = (post.description +" "+ post.title).split(/\s+/);
+        break;
+      case("chat"):
+        var textchat = ""
+        for (var i=0;i<post.dialogue.length;i++){
+           textchat += post.dialogue[i].phrase+" ";
+        }
+        line = textchat.split(/\s+/);
+        break;
+      case("video"):
+        line = post.caption.split(/\s+/);
         break;
       } 
         for (var i=0; i<post.tags.length -1;i++){
           line.push(post.tags[i]);
         }
-
         line = parsetext(line);
         for (var i =0;i<line.length-1;i++){
-          if (line[i].length>2){
-            text[line[i] = line[i].toLowerCase()]=text[line[i]]||0;
+          line[i] = line[i].toLowerCase();
+          if (line[i].length>2 && !stopwords.test(line[i])){
+            text[line[i]]=text[line[i]]||0;
             text[line[i]]++;
           }
         }
@@ -77,14 +98,16 @@ function tumbl(blogname){
 
  function parsetext(txt){
   for (var i=0;i<txt.length-1;i++){
-    txt[i].replace(/\W/g, '');
+    txt[i] = txt[i].replace(/[^\w\s'\u2019]/gi, ''); //replaces all non-word-non-space & non-apostrophe characters
+    txt[i] = txt[i].replace("\u2019", "'");
   }
   return txt;
  }
 
   function createcloud(){
     var blogname = document.getElementById("name").value + ".tumblr.com";
-    tumbl(blogname);
+    text = {};
+    tumbl(blogname,0);
     }
 
   function draw(words) {
